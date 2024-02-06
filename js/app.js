ec49d5b368db6908d4be8011cc4b19fecb689980
unknown <kadirovbrucelee@gmail.com>
2024-02-06 12:11:36 +0500

    update responsive and adaptive

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-if (ScrollTrigger.isTouch !== 1) {
+// if (ScrollTrigger.isTouch !== 1) {
 
 	ScrollSmoother.create({
-		wrapper: '.wrapper',
+		wrapper: '.wrapper',//.
 		content: '.content',
 		smooth: 1.5,
 		effects: true
-	})
+	});
 
 	gsap.fromTo('.hero-section', { opacity: 1 }, {
 		opacity: 0,
@@ -17,9 +17,9 @@ if (ScrollTrigger.isTouch !== 1) {
 			end: '820',
 			scrub: true
 		}
-	})
+	});
 
-	let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+	let itemsL = gsap.utils.toArray('.gallery__left .gallery__item');
 
 	itemsL.forEach(item => {
 		gsap.fromTo(item, { opacity: 0, x: -50 }, {
@@ -31,9 +31,9 @@ if (ScrollTrigger.isTouch !== 1) {
 				scrub: true
 			}
 		})
-	})
+	});
 
-	let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+	let itemsR = gsap.utils.toArray('.gallery__right .gallery__item');
 
 	itemsR.forEach(item => {
 		gsap.fromTo(item, { opacity: 0, x: 50 }, {
@@ -45,6 +45,14 @@ if (ScrollTrigger.isTouch !== 1) {
 				scrub: true
 			}
 		})
-	})
+	});
 
-}
+// };
+
+// typed script 
+var typed = new Typed('#typed', {
+	strings: ['OIL', 'PETROL','DIESEL'],
+	typeSpeed: 80,
+	backSpeed:80,
+	loop:true
+  });
